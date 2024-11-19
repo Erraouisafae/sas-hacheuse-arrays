51fc44c24f2d65db3027a1e37000cf671b57aada
safae <erraouisafae@gmail.com>
2024-11-19 15:25:16 +0100

    arryes

--- Exercice.js
+++ Exercice.js
@@ -1,1 +1,21 @@
-// 
+// exercices faciles 
+//ex1
+const nombres = [1 , 2 ,3 ,4 ,5];
+console.log(nombres);
+
+//ex2
+const  elements =["apple", "banana", "cherry"];
+ console.log(elements[1]);
+
+ //ex3
+ let nbrs =[10, 20, 30, 40];
+ console.log(nbrs);
+ //modifie
+  nbrs[2]=50;
+  console.log(nbrs);
+   
+  //ex4
+  const fruits=["mango", ""]
+
+
+ 
